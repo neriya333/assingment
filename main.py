1105948dea3e20e1cdb1c626d50ab33563a4b7e4
neriya333 <neriya333@cs.huji.ac.il>
2023-06-14 13:28:48 +0300

    lready done: a simple TK interface for users and TODOS. added posts, comments. add bullets. found bug next: make it look nicer

--- main.py
+++ main.py
@@ -66,10 +66,14 @@ def get_list(item_type, id, list_box):
         print('Failed to retrieve items for ID', id)
 
 
-def listbox_clicked(event, listbox_activated, listbox_write_to):
-    # Get the selected user from the listbox
-    selected_user = listbox_activated.get(tk.ACTIVE).replace(bullet_point,'').strip()
-    id = user_id[selected_user]
+def listbox_clicked(event, listbox_activated, listbox_write_to, item_type):
+    """ handle clicks by geting the clicked item name, its id, and handle the list"""
+    # Get the selected item from the listbox
+    selected_item = listbox_activated.get(tk.ACTIVE).replace(bullet_point, '').strip()
+    if item_type == USER:
+        id = user_id[selected_item]
+    else:
+        id = post_id[selected_item]
 
     # Call the get_todos function with the selected user's ID
     get_list(USER, id, listbox_write_to)
@@ -103,20 +107,25 @@ def create_toolbox(window, item_type, side):
         # Populate the listbox with the users
         for item in items:
             items_listbox.insert(tk.END, f"{bullet_point} {item[param]}")
-            user_id[item[param]] = item['id']
+            # todo there is some bug here with the dicts
+            if item_type == USER:
+                user_id[item[param]] = item['id']
+            elif item_type == POST:
+                post_id[item[param]] = item['id']
 
         # Create a listbox to display the todos
         info_listbox = tk.Listbox(frame)
         info_listbox.pack(side=tk.BOTTOM)
 
         # Bind the user_clicked function to the listbox selection event
-        items_listbox.bind('<<ListboxSelect>>', lambda event: listbox_clicked(event, items_listbox, info_listbox))
+        items_listbox.bind('<<ListboxSelect>>',
+                           lambda event: listbox_clicked(event, items_listbox, info_listbox, item_type))
 
     return frame
 
 
+"make a tk window, list frame for (users,todos), another for (posts,comments)"
 if __name__ == '__main__':
-    ""
     window = tk.Tk()
     users_frame = create_toolbox(window, USER, tk.LEFT)
     posts_frame = create_toolbox(window, POST, tk.RIGHT)
