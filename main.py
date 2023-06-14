3f4c669ad0898513db36643ec5d1163f7bedee8e
neriya333 <neriya333@cs.huji.ac.il>
2023-06-14 12:42:37 +0300

    lready done: a simple TK interface for users and TODOS. added: make 2 windows 1 by 1, added make the functions more general next: make it look nicer

--- main.py
+++ main.py
@@ -4,27 +4,27 @@
 # COMMENTS = 2
 #
 #
-# def request_key(key):
-#     if not(key == USERS or key == COMMENTS):
+# def request_key(item_type):
+#     if not(item_type == USERS or item_type == COMMENTS):
 #         raise Exception("Wrong input")
-#     if key == USERS:
+#     if item_type == USERS:
 #         requested = 'users'
 #     else:
 #         requested = 'posts'
 #
 #     # Send a GET request to the API endpoint for users
-#     response = requests.get(f'https://jsonplaceholder.typicode.com/{requested}')
+#     users_response = requests.get(f'https://jsonplaceholder.typicode.com/{requested}')
 #
 #     # Check if the request was successful (status code 200)
-#     if response.status_code == 200:
-#         # Parse the response as JSON
-#         users = response.json()
+#     if users_response.status_code == 200:
+#         # Parse the users_response as JSON
+#         users = users_response.json()
 #
 #         # Iterate over the users and print their names
 #         for user in users:
 #             print(user['name'])
 #     else:
-#         print('Failed to retrieve users. Status code:', response.status_code)
+#         print('Failed to retrieve users. Status code:', users_response.status_code)
 #
 #
 # # Press the green button in the gutter to run the script.
@@ -34,63 +34,97 @@
 import requests
 import tkinter as tk
 
+USER = 'userId'
+POST = 'postId'
 
-def get_todos(user_id):
-    # Send a GET request to the todos endpoint for the specified user ID
-    response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={user_id}')
+user_id = {}
+post_id = {}
+
+
+def get_list(item_type, id, list_box):
+    # Send a GET request to the items (todos or comments) endpoint for the specified user ID
+    if item_type == USER:
+        response = requests.get(f'https://jsonplaceholder.typicode.com/todos?{USER}={id}')
+    elif item_type == POST:
+        response = requests.get(f'https://jsonplaceholder.typicode.com/comments?{POST}={id}')
+    else:
+        raise Exception('wrong input in get_list')
 
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Parse the response as JSON
+        # Parse the users_response as JSON
         todos = response.json()
 
         # Clear the previous todos (if any)
-        todos_listbox.delete(0, tk.END)
+        list_box.delete(0, tk.END)
 
         # Add the todos to the listbox
         for todo in todos:
-            todos_listbox.insert(tk.END, todo['title'])
+            list_box.insert(tk.END, todo['title'])
     else:
-        print('Failed to retrieve todos for user ID', user_id)
+        print('Failed to retrieve items for ID', id)
 
 
-def user_clicked(event):
+def listbox_clicked(event, listbox_activated, listbox_write_to):
     # Get the selected user from the listbox
-    selected_user = users_listbox.get(tk.ACTIVE)
-    user_id = int(selected_user.split(':')[0])
+    selected_user = listbox_activated.get(tk.ACTIVE)
+    id = user_id[selected_user]
 
     # Call the get_todos function with the selected user's ID
-    get_todos(user_id)
+    get_list(USER, id, listbox_write_to)
 
 
-# Send a GET request to the users endpoint
-response = requests.get('https://jsonplaceholder.typicode.com/users')
+def create_toolbox(window, item_type, side):
+    # Send a GET request to the items endpoint
+    items_listbox = None
+    items_response = []
+    param = ''
+    if item_type == USER:
+        param = 'name'
+        items_response = requests.get('https://jsonplaceholder.typicode.com/users')
+    elif item_type == POST:
+        param = 'title'
+        items_response = requests.get('https://jsonplaceholder.typicode.com/posts')
 
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Parse the response as JSON
-    users = response.json()
+    # Create a frame for the listbox
+    frame = tk.Frame(window)
+    frame.pack(side=side)
 
-    # Create the main window
+    # Create a listbox to display the items
+    items_listbox = tk.Listbox(frame)
+    items_listbox.pack(side=tk.TOP)
+
+    # Check if the request was successful (status code 200)
+    if items_response.status_code == 200:
+        # Parse the users_response as JSON
+        items = items_response.json()
+
+        # Populate the listbox with the users
+        for item in items:
+            items_listbox.insert(tk.END, f"{item[param]}")
+            user_id[item[param]] = item['id']
+
+        # Create a listbox to display the todos
+        info_listbox = tk.Listbox(frame)
+        info_listbox.pack(side=tk.BOTTOM)
+
+        # Bind the user_clicked function to the listbox selection event
+        items_listbox.bind('<<ListboxSelect>>', lambda event: listbox_clicked(event, items_listbox, info_listbox))
+
+    return frame
+
+
+if __name__ == '__main__':
     window = tk.Tk()
+    users_frame = create_toolbox(window, USER, tk.RIGHT)
+    posts_frame = create_toolbox(window, POST, tk.LEFT)
 
-    # Create a listbox to display the users
-    users_listbox = tk.Listbox(window)
-    users_listbox.pack(side=tk.TOP)
+    users_frame.pack()
+    posts_frame.pack()
 
-    # Populate the listbox with the users
-    for user in users:
-        users_listbox.insert(tk.END, f"{user['name']}")
-
-    # Create a listbox to display the todos
-    todos_listbox = tk.Listbox(window)
-    todos_listbox.pack(side=tk.BOTTOM)
-
-    # Bind the user_clicked function to the listbox selection event
-    users_listbox.bind('<<ListboxSelect>>', user_clicked)
-
-    # Run the main window's event loop
     window.mainloop()
+    # Run the main window's event loop
+    # window.mainloop()
 
-else:
-    print('Failed to retrieve users. Status code:', response.status_code)
+    # else:
+    #     print('Failed to retrieve users. Status code:', items_response.status_code)
